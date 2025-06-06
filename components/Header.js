a0dd1e16dd2659ccf92724adf0429007e91b4aa5
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-06 14:53:04 +0200

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header><img src = "/images/troll-face.png" className = "header--image"/><h1 className = "header--title">Meme Generator</h1></header>
+    )
+}
