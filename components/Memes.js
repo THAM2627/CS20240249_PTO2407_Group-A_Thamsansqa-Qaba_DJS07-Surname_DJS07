0a377fc155845e24bb9bf48c00af1582c698ae69
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-08 19:12:53 +0200

    meme image and get function

--- components/Memes.js
+++ components/Memes.js
@@ -38,7 +38,24 @@ return (
     <main>
 
        <div className="form">
-        <input type="text" placeholder="Top text" className="form--input" name="topText" value={meme.topText} onChange={handleChange}/>
+
+        <input type = "text"  
+        placeholder = "Top text" className = "form--input" name="topText" 
+        value = {meme.topText} 
+        onChange = {handleChange}/>
+
+        <input type = "text"
+        placeholder = "Bottom text"
+        className = "form--input"
+        name = "bottomText"
+        value = {meme.bottomText}
+        onChange = {handleChange}/>
+
+        <button className = "form--button" onClick = {getMemeImage}>
+
+        Get a new meme image
+
+        </button>
         
         </div> 
         </main>
