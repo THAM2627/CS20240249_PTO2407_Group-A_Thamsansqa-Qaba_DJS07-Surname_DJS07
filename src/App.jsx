4b2a306d30e786d52825bc886934fa531c9e5b64
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-08 20:02:18 +0200

    copyin g code from memes.js over to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 function App() {
 
-  
+  const [meme, setMeme]  = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+})
+
+const [allMemes, setAllMemes] = useState([])
+
+useEffect(()=> {
+  async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();setAllMemes(data.data.memes);
+  }
+  getMeme()
+
+
+function getMemeImage() {
+  const randomNumber = Math.floor(Math.random() * allMemes.length);
+  const url = allMemes[randomNumber].url;(prevMeme=>({
+      ...prevMeme,
+      randomImage: url
+  }))
+}
 
   return (
     <div>
