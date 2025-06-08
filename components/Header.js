76165ae1efaa9fc48ab3e4463fb1666cc6eb1ca6
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-08 17:36:46 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export default function Header() {
     return (
-        <header><img src = "/images/troll-face.png" className = "header--image"/><h1 className = "header--title">Meme Generator</h1></header>
+        <header>
+            
+        <img src = "/images/troll-face.png" className = "header--image"/>
+        <h1 className = "header--title">Meme Generator</h1>
+        <h2 className= "header--project">React Course - Project 3</h2>
+        
+        </header>
     )
 }
