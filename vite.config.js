f7e8c19d8ad6cb3ef1719eca724f019e6662f620
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-08 21:14:59 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], 
-  build: {
+  plugins: [react()], build: {
     sourcemap: true
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-});
+})
