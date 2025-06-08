a9836a092c1b5fc314a151730fae8d5b94a79d3a
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-08 17:44:44 +0200

    meme function with picture

--- components/Memes.js
+++ components/Memes.js
@@ -1,3 +1,9 @@
 import React from "react";
 
-export default function Meme()
+export default function Meme() {
+    const [meme, setMeme]  = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+}
